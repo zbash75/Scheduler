6ea068521f2bc9ac87ce48b870d340fba4c78fe3
Zain Bashey <zainbashey@Zains-MacBook-Pro-2.local>
2021-04-22 21:09:26 -0500

    Add course detail screen

--- App.js
+++ App.js
@@ -1,45 +1,27 @@
-import React, {useState, useEffect} from 'react';
-import { View, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, ViewScrollView } from 'react-native';
-import CourseList from './components/CourseList';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ScheduleScreen from './screens/ScheduleScreen';
+import CourseDetailScreen from './screens/CourseDetailScreen';
 
-
-const Banner = ({title}) => (
-  <Text style={styles.bannerStyle}>{title}</Text>
-);
-
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-
-  useEffect(() => {
-    const fetchSchedule =  async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, []);
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
-    </SafeAreaView>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ScheduleScreen"
+          component={ScheduleScreen}
+          options={{title: 'Schedule'}}
+        />
+        <Stack.Screen name="CourseDetailScreen"
+          component={CourseDetailScreen}
+          options={{ title: 'Course detail'}} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bannerStyle: {
-    color: '#888',
-    fontSize: 32,
-  }
-});
 
 export default App;
--- components/CourseSelector.js
+++ components/CourseSelector.js
@@ -3,7 +3,7 @@ import {View, StyleSheet} from 'react-native';
 import Course from './course';
 import {getCourseNumber, getCourseTerm, hasConflict, terms } from '../utils/course';
 
-const CourseSelector = ({courses}) => {
+const CourseSelector = ({courses, view}) => {
     const [selected, setSelected] = useState([]);
 
     const toggle = course => setSelected(selected => (
@@ -18,6 +18,7 @@ const CourseSelector = ({courses}) => {
               isDisabled={hasConflict(course, selected)}
               isSelected={selected.includes(course)}
               select={toggle}
+              view={view}
             />
           ))
         }
